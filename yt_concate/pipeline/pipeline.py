435177bb6acc458b20fa353179269a6a952846bf
David Hou <r09921082@g.ntu.edu.tw>
2022-07-05 12:41:30 +0800

    Refactor code into pipeline design pattern

---
+++ yt_concate/pipeline/pipeline.py
@@ -0,0 +1,18 @@
+from yt_concate.pipeline.steps.step import StepException
+
+
+# 建立一個自動化工廠
+class Pipeline:
+    def __init__(self, steps):
+        self.steps = steps
+
+    def run(self, inputs):
+        data = None
+
+        # 利用for迴圈逐一跑它
+        for step in self.steps:
+            try:
+                data = step.process(data, inputs)
+            except StepException as e:  # 抓每個step中的錯誤
+                print("Exception happened: ", e)
+                break
---
+++ yt_concate/pipeline/steps/step.py
@@ -0,0 +1,19 @@
+from abc import ABC  # abstract base class
+from abc import abstractmethod
+
+
+# Base class(Abstract class)
+class Step(ABC):
+
+    def __init__(self):
+        pass
+
+    @abstractmethod  # decorator, 子類別一定要寫這個method
+    # 設計介面
+    # inputs 為一個 dict
+    def process(self, data, inputs):
+        pass
+
+
+class StepException(Exception):
+    pass
